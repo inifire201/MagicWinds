33b1fa61244f55a2c324e75875d51dd3b120fd69
Rob de Witte <inifire201@gmail.com>
2017-05-25 20:37:29 +0200

    Blocks Drop Shards
    Signed-off-by: Rob de Witte <inifire201@gmail.com>

--- src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOreOrange.java
+++ src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOreOrange.java
@@ -1,7 +1,12 @@
 package com.inifire201.MagicWinds.Blocks;
 
+import com.inifire201.MagicWinds.Handlers.ItemHandler;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 /**
  * Created by Rob de Witte on 25-5-2017.
@@ -10,6 +15,12 @@ import net.minecraft.creativetab.CreativeTabs;
 public class BlockShardOreOrange extends BlockShardOre{
     public BlockShardOreOrange(Material mat, String name, CreativeTabs tab, float hardness, float resistance, int harvest, String tool) {
         super(mat, name, tab, hardness, resistance, harvest, tool);
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return ItemHandler.orangeShard;
     }
 
 
