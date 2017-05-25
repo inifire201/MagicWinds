6a3a53ce943125531d78ee77f8cca9d4604749a0
Patrick Rienstra <patrick@rienstra.biz>
2017-05-25 21:54:43 +0200

    Coded 2 new items

---
+++ src/main/java/com/inifire201/MagicWinds/Items/ItemShardPurple.java
@@ -0,0 +1,12 @@
+package com.inifire201.MagicWinds.Items;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+/**
+ * Created by patrick on 5/25/17.
+ */
+public class ItemShardPurple  extends ItemShard{
+    public ItemShardPurple(String name, CreativeTabs tab, int stacksize) {
+        super(name, tab, stacksize);
+    }
+}
