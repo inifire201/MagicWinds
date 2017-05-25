eb45184d9f51918068d07a822eb754621d22e68b
Rob de Witte <inifire201@gmail.com>
2017-05-25 20:37:29 +0200

    Blocks Drop Shards
    Signed-off-by: Rob de Witte <inifire201@gmail.com>

--- src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOre.java
+++ src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOre.java
@@ -1,7 +1,10 @@
 package com.inifire201.MagicWinds.Blocks;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+
+import java.util.Random;
 
 /**
  * Created by Rob de Witte on 25-5-2017.
@@ -9,10 +12,13 @@ import net.minecraft.creativetab.CreativeTabs;
  */
 public class BlockShardOre extends ModBlock {
 
-    public BlockShardOre(Material mat, String name, CreativeTabs tab, float hardness, float resistance, int harvest, String tool) {
+    BlockShardOre(Material mat, String name, CreativeTabs tab, float hardness, float resistance, int harvest, String tool) {
         super(mat, name, tab, hardness, resistance, harvest, tool);
     }
 
-
+    @Override
+    public int quantityDropped(IBlockState state, int fortune, @SuppressWarnings("NullableProblems") Random random) {
+        return (((random.nextInt(3)+1)*(fortune))+1);
+    }
 
 }
--- src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOreGreen.java
+++ src/main/java/com/inifire201/MagicWinds/Blocks/BlockShardOreGreen.java
@@ -1,7 +1,12 @@
 package com.inifire201.MagicWinds.Blocks;
 
+import com.inifire201.MagicWinds.Handlers.ItemHandler;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 /**
  * Created by Rob de Witte on 25-5-2017.
@@ -10,5 +15,11 @@ import net.minecraft.creativetab.CreativeTabs;
 public class BlockShardOreGreen extends BlockShardOre {
     public BlockShardOreGreen(Material mat, String name, CreativeTabs tab, float hardness, float resistance, int harvest, String tool) {
         super(mat, name, tab, hardness, resistance, harvest, tool);
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+            return ItemHandler.greenShard;
     }
 }
