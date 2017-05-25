aa5b5a9fe2d9ea9dc8fdd81c020ae3958c9a5d01
Rob de Witte <inifire201@gmail.com>
2017-05-25 19:58:49 +0200

    Green shard
    Signed-off-by: Rob de Witte <inifire201@gmail.com>

--- src/main/java/com/inifire201/MagicWinds/Items/ItemShard.java
+++ src/main/java/com/inifire201/MagicWinds/Items/ItemShard.java
@@ -6,8 +6,8 @@ import net.minecraft.creativetab.CreativeTabs;
  * Created by Rob de Witte on 25-5-2017.
  * Package com.inifire201.MagicWinds.Items.
  */
-public class ItemShardOrange extends ItemShard{
-    public ItemShardOrange(String name, CreativeTabs tab, int stacksize) {
+public class ItemShard extends ModItem {
+    public ItemShard(String name, CreativeTabs tab, int stacksize) {
         super(name, tab, stacksize);
     }
 }
